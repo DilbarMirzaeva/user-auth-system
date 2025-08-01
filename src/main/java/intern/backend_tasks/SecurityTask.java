c867e5b93154317283b6edf38e2089b6def2b6c9
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-08-01 23:59:30 +0400

    JwtFilter and SecurityConfig

--- src/main/java/intern/backend_tasks/SecurityTask.java
+++ src/main/java/intern/backend_tasks/SecurityTask.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendTasksApplication {
+public class SecurityTask {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendTasksApplication.class, args);
+        SpringApplication.run(SecurityTask.class, args);
     }
 
 }
--- src/main/java/intern/backend_tasks/config/SecurityConfig.java
+++ src/main/java/intern/backend_tasks/config/SecurityConfig.java
@@ -1,5 +1,49 @@
 package intern.backend_tasks.config;
 
+import intern.backend_tasks.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth->auth
+                                .requestMatchers("/login").permitAll()
+                                .anyRequest().authenticated()
+                        )
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(userDetailsService)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
 }
