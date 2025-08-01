754bdb019add5756cd012e581747546e0fa7c12d
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-08-02 01:52:30 +0400

    userDetails && add method UserRepository

---
+++ src/main/java/intern/backend_tasks/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package intern.backend_tasks.service;
+
+import intern.backend_tasks.domain.entity.User;
+import intern.backend_tasks.domain.repository.UserRepository;
+import io.jsonwebtoken.lang.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
