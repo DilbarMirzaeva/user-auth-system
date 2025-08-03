2afb58f6aebe3f5b9f79622830a43278221b27a7
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-08-03 21:02:03 +0400

    JwtUtil

--- src/main/java/intern/backend_tasks/security/JwtUtil.java
+++ src/main/java/intern/backend_tasks/security/JwtUtil.java
@@ -1,4 +1,61 @@
 package intern.backend_tasks.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public Claims getClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token){
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            return !isTokenExpired(token);
+        }catch (Exception e){
+            return false;
+        }
+    }
+
 }
