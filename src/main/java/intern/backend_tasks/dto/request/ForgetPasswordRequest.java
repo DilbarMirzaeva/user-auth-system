57458c73f11774d39785d5dbbf806f73f371c720
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-08-04 01:34:57 +0400

    forget and reset dto

--- src/main/java/intern/backend_tasks/dto/request/ForgetPasswordRequest.java
+++ src/main/java/intern/backend_tasks/dto/request/ForgetPasswordRequest.java
@@ -1,4 +1,12 @@
 package intern.backend_tasks.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ForgetPasswordRequest {
+    private String email;
 }
