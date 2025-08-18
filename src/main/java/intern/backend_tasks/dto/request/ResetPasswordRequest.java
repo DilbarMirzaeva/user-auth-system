608aa347e8d23db3fa0ad7d08a13391bb7b9c15b
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-08-18 23:54:41 +0400

    fix requests

--- src/main/java/intern/backend_tasks/dto/request/ResetPasswordRequest.java
+++ src/main/java/intern/backend_tasks/dto/request/ResetPasswordRequest.java
@@ -1,5 +1,8 @@
 package intern.backend_tasks.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResetPasswordRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
-    private String verificationCode;
+
+    @NotBlank
+    @Size(min = 8,message = "password must be at least 8 characters long")
     private String newPassword;
+
+    private String verificationCode;
 }
