8bdca76a223d4f253ab59db68ba9cd0b58c53ce3
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-08-30 22:47:21 +0400

    add authority

--- src/main/java/intern/backend_tasks/service/CustomUserDetailsService.java
+++ src/main/java/intern/backend_tasks/service/CustomUserDetailsService.java
@@ -2,12 +2,15 @@ package intern.backend_tasks.service;
 
 import intern.backend_tasks.domain.entity.User;
 import intern.backend_tasks.domain.repository.UserRepository;
-import io.jsonwebtoken.lang.Collections;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -19,10 +22,13 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user=userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        GrantedAuthority authority=new SimpleGrantedAuthority("ROLE_"+user.getRole());
+
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(),
                 user.getPassword(),
-                Collections.emptyList()
+                Collections.singletonList(authority)
         );
     }
 }
