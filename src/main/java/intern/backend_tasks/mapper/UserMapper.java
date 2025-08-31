8e18542fafe771c65bde58fdbec5a8da2c204435
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-09-01 00:58:56 +0400

    userMapper fixed

--- src/main/java/intern/backend_tasks/mapper/UserMapper.java
+++ src/main/java/intern/backend_tasks/mapper/UserMapper.java
@@ -1,13 +1,18 @@
 package intern.backend_tasks.mapper;
 
 import intern.backend_tasks.domain.entity.User;
+import intern.backend_tasks.dto.request.UpdateUserRequest;
 import intern.backend_tasks.dto.response.UserResponse;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper extends EntityMapper<User,UserResponse> {
-    @Override
+public interface UserMapper {
      User toEntity(UserResponse userResponse);
-    @Override
      UserResponse toDto(User user);
+
+     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+     void updateUser(UpdateUserRequest updateUserRequest,@MappingTarget User user);
 }
