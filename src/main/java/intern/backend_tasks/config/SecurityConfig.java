64137efbb4957f2ce51420ecdfc098a89725dd1c
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-09-06 23:33:01 +0400

    SecurityConfig

--- src/main/java/intern/backend_tasks/config/SecurityConfig.java
+++ src/main/java/intern/backend_tasks/config/SecurityConfig.java
@@ -27,7 +27,11 @@ public class SecurityConfig {
         return http
                 .csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth->auth
-                                .requestMatchers("/api/v1/auth/*").permitAll()
+                                .requestMatchers("/api/v1/auth/*",
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources/**",
+                                        "/webjars/**").permitAll()
                                 .anyRequest().authenticated()
                         )
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
